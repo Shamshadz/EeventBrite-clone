ed7babed5e896f642d1fb45e56bd7afa6572bc78
Shamshadz <chaudhary.s.shamshad07@gmail.com>
2022-10-21 16:41:44 +0530

    update

--- app/views.py
+++ app/views.py
@@ -3,10 +3,15 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from app.models import Event, Like
 import json
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     events = Event.objects.all()
+
+    if request.user.is_authenticated:
+        customer = request.user
+        likes = Like.objects.filter(customer=customer)
 
     context = {'events':events}
     return render(request,'app/home.html',context)
@@ -22,27 +27,36 @@ def likes(request):
             likes = Like.objects.filter(customer=customer)
 
             if likes:
-                for like in likes:
-                            try:
-                                likedEvent = Like.objects.get(event_id=eventId)
-                                likedEvent.delete()
-                                liked = False
-                            except:
-                                likedEvent = Like.objects.create(customer=customer,event_id=eventId)
-                                liked=True
+                # for like in likes:
+                try:
+                    likedEvent = Like.objects.get(event_id=eventId)
+                    Event.objects.filter(id=eventId).update(is_liked='False')
+                    likedEvent.delete()
+                    liked = False
+                except:
+                    likedEvent = Like.objects.create(customer=customer,event_id=eventId)
+                    Event.objects.filter(id=eventId).update(is_liked='True')
+                    liked=True
             else:
                 likedEvent = Like.objects.create(customer=request.user,event_id=eventId)
+                Event.objects.filter(id=eventId).update(is_liked='True')
                 liked=True
 
-            ctx={"liked":liked,'eventId':eventId}
+            ctx={"liked":liked,'eventId':eventId,'login':True}
             return HttpResponse(json.dumps(ctx), content_type='application/json')
+
+        else:
+            ctx={'login':False}
+            return HttpResponse(json.dumps(ctx), content_type='application/json')
+
 
     if request.user.is_authenticated:
             customer = request.user
-            likes = Like.objects.filter(customer=customer)
+            likedEvt = Like.objects.filter(customer=customer)
 
-
-    context={'likes':likes}
+            context={'likedEvt':likedEvt}
+    else:
+        context={}
 
     return render(request,'app/likes.html',context)
 
